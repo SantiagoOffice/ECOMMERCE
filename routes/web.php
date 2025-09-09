4f5991796e14c1ba1145f9dc65df91057b0f82bc
sgomez262 <sgomez262@unab.edu.co>
2025-09-09 19:17:09 +0000

    Controlador Básico

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,14 @@
 <?php
-
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', function () {
-    return "List products";
-});
+Route::get('/products', [ProductController::class, 'index']);
 
-Route::get('/products/{id}/{category?}', function ($id, $category = null) {
+Route::get('/products/create', [ProductController::class, 'create']);
 
-    if ($category != null) {
-        return "Detail products: " . $id . ". With Category: " . $category;
-    } else {
-        return "Detail products: " . $id;
-    }
-});
-
+Route::get('/products/{id}/{category?}', [ProductController::class, 'detail']);
 
